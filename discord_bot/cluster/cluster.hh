ed60c8015b843ec4058b06e41298ad45f68b0f83
Glsswrks <nara04542@gmail.com>
2025-05-19 21:26:13 +0700

    Made some changes

--- discord_bot/cluster/cluster.hh
+++ discord_bot/cluster/cluster.hh
@@ -16,8 +16,20 @@ namespace cluster
 {
 	class config {
 	public:
+		config(uint64_t id, std::string str) {
+			serverID = id;
+			token = str;
+		}
+		auto swearwords() -> const std::unordered_set<std::string>& {
+			return swear_words;
+		}
+		void insert(const std::string& str) {
+			swear_words.insert(str);
+		}
+	public:
 		uint64_t serverID;
 		std::string token;
+	private:
 		std::unordered_set<std::string> swear_words = {};
 	};
 
@@ -30,14 +42,14 @@ namespace cluster
 		const discord_bot* initialize();
 	public:
 		void ready_t(const dpp::ready_t& ctx);
-		void interaction_create_t(const dpp::interaction_create_t& ctx);
+		void message_create_t(const dpp::message_create_t& ctx);
 	public:
 		void send_command(commands::command_t& cmd);
 	public:
 		bool is_swearwords(const std::string& content);
 		bool is_command(const std::string& content);
 	private:
-		std::pair<dpp::cluster*, config> m_cluster = {};
+		std::pair<dpp::cluster*, config> m_cluster = { nullptr, {0x0, ""} };
 	};
 }
 #endif __CLUSTER_HH__ // !__CLUSTER_HH__
